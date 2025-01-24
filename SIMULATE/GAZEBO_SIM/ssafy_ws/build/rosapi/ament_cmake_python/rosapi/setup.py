b43b34064bcd5cd5d9675cac000bdbad95b31890
서영은 <sye7283@gmail.com>
2025-01-24 15:11:46 +0900

    test:backend->develop merge conflict solve test

--- SIMULATE/GAZEBO_SIM/ssafy_ws/build/rosapi/ament_cmake_python/rosapi/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='rosapi',
-    version='2.0.0',
-    packages=find_packages(
-        include=('rosapi', 'rosapi.*')),
-)
