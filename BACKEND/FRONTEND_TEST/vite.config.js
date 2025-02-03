d54e13d609a53d5ff7aa8f21d0e2985c895509b7
서영은 <sye7283@gmail.com>
2025-02-03 14:42:02 +0900

    Revert "refactor: 백엔드, 프론트엔드 테스트 배포 확인용"
    This reverts commit 7063f6f8df52a919eeabeddd58e3dfd9c5040048

--- BACKEND/FRONTEND_TEST/vite.config.js
+++ BACKEND/FRONTEND_TEST/vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -10,7 +9,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
   server: {
