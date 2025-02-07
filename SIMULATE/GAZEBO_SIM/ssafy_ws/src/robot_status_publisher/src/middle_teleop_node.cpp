241b9244d94060d19556bf974138025a63f9b072
신현학 <s01087350661@gmail.com>
2025-02-07 14:10:53 +0900

    fix: manual모드 아니라는 로그 너무많이떠서 시간초검

--- SIMULATE/GAZEBO_SIM/ssafy_ws/src/robot_status_publisher/src/middle_teleop_node.cpp
+++ SIMULATE/GAZEBO_SIM/ssafy_ws/src/robot_status_publisher/src/middle_teleop_node.cpp
@@ -147,7 +147,12 @@ private:
         cmd_vel_publisher_->publish(cmd_vel_msg);
     }
     else{
-        RCLCPP_ERROR(this->get_logger(), "Current Mode is not Manual mode");
+        static auto last_error_log_time = std::chrono::steady_clock::now();
+        auto current_time = std::chrono::steady_clock::now();
+        if (std::chrono::duration_cast<std::chrono::seconds>(current_time - last_error_log_time).count() >= 1) {
+            RCLCPP_INFO(this->get_logger(), "Current Mode is not Manual mode");
+            last_error_log_time = current_time;
+        }
     }
   }
   bool is_manual_mode = false;
