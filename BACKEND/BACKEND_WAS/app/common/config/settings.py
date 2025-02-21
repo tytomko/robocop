638cfcc1d59158c959350f3c0e7d2fd5833cea4d
Ubuntu <ubuntu@ip-172-26-12-3.ap-northeast-2.compute.internal>
2025-02-21 15:29:37 +0900

    test:merge test
    Former-commit-id: e131c33ad201cf90f707b5047b3cd656508c7aeb

--- BACKEND/BACKEND_WAS/app/common/config/settings.py
+++
@@ -1,8 +0,0 @@
-class ROS2Settings(BaseSettings):
-    BRIDGE_URL: str = "ws://127.0.0.1:10000"
-    RETRY_INTERVAL: int = 5  # 재연결 시도 간격 (초)
-    MAX_RETRIES: int = 3     # 최대 재연결 시도 횟수
-
-class Settings(BaseSettings):
-    # ... 기존 설정들 ...
-    ros2: ROS2Settings = ROS2Settings() 
--- BACKEND/BACKEND_WAS/rosbridge/ros_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# setup ros environment
-source "/opt/ros/humble/setup.bash"
-source "/ros2_ws/install/setup.bash"
-
-exec "$@"
-
